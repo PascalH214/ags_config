9cc789091267afc1cc4d3f3b1c1f89ac1b3949b8
PascalH214 <pascal02012004@freenet.de>
2026-03-01 19:20:02 +0100

    Add click to open special-workspcae

--- widget/Bar/Workspaces.tsx
+++ widget/Bar/Workspaces.tsx
@@ -2,6 +2,7 @@ import Hyprland from "gi://AstalHyprland"
 import { createBinding, createEffect, createState, For } from "gnim"
 import { Gtk } from "ags/gtk4"
 import GObject from "gnim/gobject"
+import Gio from "gi://Gio?version=2.0"
 
 const hyprland = Hyprland.get_default()
 
@@ -29,7 +30,16 @@ export default function Workspaces(props: Partial<Gtk.Box.ConstructorProps> = {}
         <button
           class={id == focusedWorkspaceId() ? "focused" : ""}
           label={name}
-          onClicked={() => id != focusedWorkspaceId() && workspace.focus()}
+          onClicked={() => {
+            if (name == "S") {
+              const process = Gio.Subprocess.new(["hyprctl", "dispatch", "togglespecialworkspace", "magic"], Gio.SubprocessFlags.NONE);
+              process.wait(null);
+              return;
+            }
+
+            id != focusedWorkspaceId();
+            workspace.focus()
+          }}
         />
       )
     })
